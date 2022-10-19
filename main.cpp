efc3975dafd8323dc9f515f202efec25c074abfe
asreed666 <areed@cityplym.ac.uk>
2022-10-19 09:17:08 +0000

    added tehrmistor voltage measurement

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@
  */
 
 #include "mbed.h"
+#include "stdio.h"
 
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     200ms
+#define BLINKING_RATE     2s
+#define THERM_OUT         P10_1
+#define THERM_VDD         P10_3
+#define THERM_GND         P10_0
 
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+    DigitalOut vdd(THERM_VDD);
+    DigitalOut gnd(THERM_GND);
+    AnalogIn tempVoltage(THERM_OUT);
 
     while (true) {
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
+        vdd = true;
+        gnd = false;
+        float tempTherm = tempVoltage;
+        vdd = false;
+        printf("The thermistor voltage is %fVolts\n", tempTherm);
     }
 }
